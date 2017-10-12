4cd89b4ce5f0e48a79d1fcee4fe22478d7127b0c
chendongdong <chendongdong@syswin.com>
2017-10-12 16:41:29 +0800

    修复bug 添加忽略

---
+++ CWKit/Foundation/NSString/NSString+Search.h
@@ -0,0 +1,12 @@
+//
+//  NSString+Search.h
+//  CWKit
+//
+//  Created by syswin on 2017/10/12.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Search)
+
+@end
---
+++ CWKit/Foundation/NSString/NSString+Search.m
@@ -0,0 +1,12 @@
+//
+//  NSString+Search.m
+//  CWKit
+//
+//  Created by syswin on 2017/10/12.
+//
+
+#import "NSString+Search.h"
+
+@implementation NSString (Search)
+
+@end
